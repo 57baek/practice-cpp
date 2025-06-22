8c322588fa800bffb390163f53e9d7ac04f09f6a
HyangMok Baek <57baek@gmail.com>
2025-06-21 23:26:27 -0400

    4th commit

--- test4.cpp
+++ test4.cpp
@@ -3,9 +3,12 @@
 
 int main() {
     std::string s = "Hello";
-    int i = 3;
+    unsigned int i1 = 4294967295;
+    int i2 = 2147483647;
     char c = 'H';
     std::cout << sizeof(s) << '\n';
-    std::cout << sizeof(i) << '\n';
+    std::cout << sizeof(i1) << '\n';
     std::cout << sizeof(c) << '\n';
+
+    std::cout << &i2 << std::endl;
 }
